5a4b86d633ca609908ada97f766a2c56e0767c1b
Elias <eliasventura23@gmail.com>
2022-12-02 20:47:53 -0500

    created frontend react

--- src/App.js
+++ src/App.js
@@ -2,6 +2,10 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  fetch("http://localhost:9292/test")
+  .then((r) => r.json())
+  .then((data) => console.log(data));
   return (
     <div className="App">
       <header className="App-header">
