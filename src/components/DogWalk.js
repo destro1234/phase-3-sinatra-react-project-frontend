5f085c5d7c6ce322e97f78124b60f19860ddab83
Elias <eliasventura23@gmail.com>
2022-12-05 09:40:48 -0500

    add time type to controlled form inputs

--- src/components/DogWalk.js
+++ src/components/DogWalk.js
@@ -5,11 +5,14 @@ import React, { useState } from 'react'
 function DogWalk() {
     const [dogname, setDogname] = useState("")
     const [dogWalker, setDogWalker] = useState("")
-    const [dogWalk, setDogWalk] = useState("")
+    const [startTime, setStartTime] = useState("")
+    const [endTime, setEndTime] = useState("")
+
 
     console.log(dogname)
     console.log(dogWalker)
-    console.log(dogWalk)
+    console.log(startTime)
+    console.log(endTime)
 
     function addName(event) {
         setDogname(event.target.value)
@@ -19,8 +22,12 @@ function DogWalk() {
         setDogWalker(event.target.value)
     }
 
-    function addWalk(event) {
-        setDogWalk(event.target.value)
+    function addStartTime(event) {
+        setStartTime(event.target.value)
+    }
+
+    function addEndTime(event) {
+        setEndTime(event.target.value)
     }
 
     return (
@@ -31,9 +38,9 @@ function DogWalk() {
           <label>DogWalker: </label>
           <input onChange={addDogWalker} type="text" placeholder="dogwalker"></input>
           <label>Start Time: </label>
-          <input onChange={addWalk} type="text" placeholder="starttime"></input>
+          <input onChange={addStartTime} type="time" placeholder="starttime"></input>
           <label>End Time: </label>
-          <input type="text" placeholder="endtime"></input>
+          <input onChange={addEndTime} type="time" placeholder="endtime"></input>
           <button type="submit">Submit</button>
         </form>
         <div>This is a DogWalk</div>
